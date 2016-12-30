837d9e42f5e46f2e14665a8afd43d844b662048f
edisonskiploop <edison@skiploop.com>
2016-12-30 15:39:00 +0530

    CSS Changes

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 angular.module('slautomations', [])
 .controller('mainCtrl', function ($scope) {
     $scope.active = 0;
+    $scope.services = [{
+        name : 'SMART HOME SOLUTION'
+    },{
+        name : 'SECURITY SYSTEMS'
+    },{
+        name : 'COMMON AREA LIGHTING CONTROL'
+    },{
+        name : 'ALARM AND MONITORING'
+    },{
+        name : 'BUILDING MANAGEMENT SYSTEM'
+    }];
 
     $scope.formNotValid = function () {
         if($scope.myForm.name.$valid && $scope.myForm.email.$valid && $scope.myForm.subject.$valid && $scope.myForm.message.$valid)
