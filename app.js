06f31de2d8923157d905ae9fd3adc6b677f6bcc0
edisonskiploop <edison@skiploop.com>
2016-12-30 16:29:41 +0530

    Service Font Icons Changed

--- app.js
+++ app.js
@@ -2,15 +2,20 @@ angular.module('slautomations', [])
 .controller('mainCtrl', function ($scope) {
     $scope.active = 0;
     $scope.services = [{
-        name : 'SMART HOME SOLUTION'
+        name : 'SMART HOME SOLUTION',
+        icon : 'fa-home'
     },{
-        name : 'SECURITY SYSTEMS'
+        name : 'SECURITY SYSTEMS',
+        icon : 'fa-lock'
     },{
-        name : 'COMMON AREA LIGHTING CONTROL'
+        name : 'COMMON AREA LIGHTING CONTROL',
+        icon : 'fa-lightbulb-o'
     },{
-        name : 'ALARM AND MONITORING'
+        name : 'ALARM AND MONITORING',
+        icon : 'fa-bell'
     },{
-        name : 'BUILDING MANAGEMENT SYSTEM'
+        name : 'BUILDING MANAGEMENT SYSTEM',
+        icon : 'fa-building'
     }];
 
     $scope.formNotValid = function () {
