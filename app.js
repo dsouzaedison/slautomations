2ab386c9533e314bf77667288f36160148cd2c64
edisonskiploop <edison@skiploop.com>
2016-12-30 14:23:33 +0530

    Form Validation

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 angular.module('slautomations', [])
 .controller('mainCtrl', function ($scope) {
     $scope.active = 0;
+
+    $scope.formNotValid = function () {
+        if($scope.myForm.name.$valid && $scope.myForm.email.$valid && $scope.myForm.subject.$valid && $scope.myForm.message.$valid)
+            return false;
+        else return true;
+    };
 });
